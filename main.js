4f83c61f1a1d405d675d65bf251a470ba12c6ca2
jua15377 <jua15377@uvg.edu.gt>
2018-11-11 22:54:43 -0600

    funcionalidad , aun tiene un pequenio error

--- main.js
+++ main.js
@@ -1,15 +1,48 @@
 //set up the server
 const server = require('http').createServer()
 const io = require('socket.io')(server)
-
+const FormData = require('form-data')
+const fetch = require("node-fetch")
+const axios = require('axios')
+const url = 'http://34.210.35.174:7000/'
+var total_msj = 0
 // this function will handle the connections
-io.on('connection', function (client) {
-	console.log("new connection from", client.id)
-  client.on('send_message', function(msj){
-	console.log(msj)
-  })
+io.on('connection', async function (client) {
+  console.log("new connection from", client.id)
+  // send message to the the API
+  try{
+    client.on('send_message', function(st_id, nick, text){
+      // setting data
+      total_msj++
+      var data = new FormData()
+      data.append('student_id', st_id)
+      data.append('text', text)
+      data.append('nick', nick)
+      const otherParam={
+        method:"POST",
+        body: data
+      }
+      fetch(url,otherParam)
+    })
+  }
+  catch(e) {
+		console.error(e)
+  }
 
-// default for this cases
+
+  // sent all the past messages to the client
+  try{
+      const response = await axios.get(url)
+      total_msj = response.data.length
+      console.log("sending past messages")
+      client.emit("first_conn", response.data)
+    
+  }
+  catch(e) {
+		console.error(e)
+  }
+
+// default for cases
   client.on('disconnect', function () {
     console.log('client disconnect...', client.id)
   })
@@ -21,8 +54,26 @@ io.on('connection', function (client) {
 })
 
 // server listen on port 3010
-server.listen(3010, function (err) {
+server.listen(3011, function (err) {
   if (err) throw err
-  console.log('listening on port 3010')
+  console.log('listening on port 3011')
 })
+
+setInterval(async function() {
+  // method to be executed
+  try {
+    const response = await axios.get(url)
+    const messages = response.data
+    if (messages.length !== total_msj) {
+      total_msj = messages.length
+      console.log("new message found:", messages[messages.length - 1])
+      io.emit('new_message', messages[messages.length - 1])
+    }
+  } 
+  catch(err) {
+    console.error(err)
+  }
+}, 500)
+
+
 
